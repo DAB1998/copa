dea719e36b8f5eba160950da3cabfb34756271c9
DanielAguilarBarrios <daniluko2009@hotmail.com>
2021-09-01 00:08:06 -0500

    otro2

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -51,7 +51,7 @@
                         border-radius: 50%;
                         width: 3em;
                         height: 3em;
-                        animation: spin 1s linear infinite;
+                        animation: spin 1s linear;
                     }
                     @keyframes spin {
                     to {
